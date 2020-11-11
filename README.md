97f57bc5297ac0342e0d530b6674d4f98404a318
Vladimir Logachev <logachev.dev@ya.ru>
2020-11-11 16:19:25 +0700

    Solve ch03 with manually

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # postgres-lessons
+
+Файлы баз, совместимые с книгой:
+
+https://edu.postgrespro.ru/demo-small-20161013.zip
+https://edu.postgrespro.ru/demo-medium-20161013.zip
+https://edu.postgrespro.ru/demo-big-20161013.zip
+
+```sh
+docker-compose up
+
+# psql
+docker-compose exec db psql -U admin -d demo
+```
---
+++ exercises/ch03.sql
@@ -0,0 +1,56 @@
+/*
+1
+ */
+-- ERROR:  duplicate key value violates unique constraint "aircrafts_pkey"
+INSERT INTO aircrafts
+  VALUES ('SU9', 'Sukhoi SuperJet-100', 3000);
+
+-- OK
+INSERT INTO aircrafts
+  VALUES ('SU1', 'Sukhoi SuperJet-100', 3000);
+
+
+/*
+2
+
+Самостоятельно напишите команду для выборки всех строк из таблицы aircrafts, 
+чтобы строки были упорядочены по убыванию значения 
+атрибута «Максимальная дальность полета, км» (range)
+ */
+SELECT
+  *
+FROM
+  aircrafts
+ORDER BY
+  RANGE DESC;
+
+
+/*
+3
+
+Самостоятельно напишите команду UPDATE полностью, при
+этом не забудьте, что увеличить дальность полета нужно только у одной модели — Sukhoi SuperJet,
+поэтому необходимо использовать условие WHERE. Затем
+с помощью команды SELECT проверьте полученный результат
+ */
+UPDATE
+  aircrafts
+SET
+  RANGE = RANGE * 2
+WHERE
+  aircraft_code = 'SU9';
+
+
+/*
+4
+
+Если в предложении WHERE команды DELETE вы укажете логически и синтаксически корректное условие, 
+но строк, удовлетворяющих этому условию, в таблице не окажется, то в ответ СУБД выведет сообщение
+DELETE 0
+
+Самостоятельно смоделируйте описанную ситуацию, подобрав условие, которому гарантированно
+не соответствует ни одна строка в таблице «Самолеты» (aircrafts).
+ */
+DELETE FROM aircrafts
+WHERE aircraft_code = 'SU1';
+
